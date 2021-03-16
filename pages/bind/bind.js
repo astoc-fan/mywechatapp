feb1c0a79fb4d2cef9e51010e93674cba758b7dd
astoc-fan <astoc.fan@gmail.com>
2021-03-16 22:33:01 +0800

    databind

---
+++ pages/bind/bind.js
@@ -0,0 +1,74 @@
+// pages/bind/bind.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    message:"new message!",
+  },
+
+  changeData:function(){
+    //获取数据
+    console.log(this.data.message);
+
+    // 修改数据
+    this.setData({message:"big message!"})
+
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
