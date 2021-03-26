9ceb77b481a7f01eb45987f55200bec99c9245e5
astoc-fan <astoc.fan@gmail.com>
2021-03-26 22:45:58 +0800

    upload success

--- pages/upload2/upload2.js
+++ pages/upload2/upload2.js
@@ -5,7 +5,52 @@ Page({
    * 页面的初始数据
    */
   data: {
+    img_l:''
+  },
 
+  chooseImg:function(){
+    var _this = this;
+    wx.chooseImage({
+      count: 9, // 默认9
+      sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+      sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+      success: function (res) {
+        // 返回选定照片的本地文件路径列表，tempFilePath可以作为img标签的src属性显示图片
+        var tempFilePaths = res.tempFilePaths;
+        console.log(res)
+      _this.setData({
+        img_l:res.tempFilePaths
+      })
+        console.log(res)
+      
+      }
+    })
+  },
+  up_img:function() {
+    var _this = this;
+     wx.uploadFile({
+      //  url: 'http://127.0.0.1/m_pro/upload.php', //接口
+       url:'https://www.astoc.tk:4431/mo/upload/upload.php',
+       filePath: _this.data.img_l[0],
+       name: 'file',
+       formData: {
+         'user': 'test'
+       },
+       success: function (res) {
+         var data = res.data;
+         console.log(data);
+         //do something
+       },
+       fail: function (error) {
+         console.log(error);
+       }
+     })
+   },
+   preview_img:function(){
+    wx.previewImage({
+      current: this.data.img_l, // 当前显示图片的http链接
+      urls: this.data.img_l // 需要预览的图片http链接列表
+    })
   },
 
   /**
