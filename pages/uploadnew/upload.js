bd0d2947469c036b6a715dea8a5f5138f5f1d8bc
Stef Fan <stef.fan@expeditors.com>
2021-03-30 16:21:00 +0800

    upload new update form

--- pages/uploadnew/upload.js
+++ pages/uploadnew/upload.js
@@ -2,56 +2,71 @@
 import Toast from '../../miniprogram_npm/@vant/weapp/toast/toast';
 
 Page({
-  /**
-   * 页面的初始数据
-   */
+  /*** 页面的初始数据 */
   data: {
     fileList: [], // 预览图片用的数组
     file_num: '',
     showPopup: false,
     pickvalue: "",
-    conditionOptions: [ '普通货物照片','破损货物照片','签收照片','其他照片'],
+    conditionOptions: ['Cargo Photo', 'OSD Photo', 'DLV Photo', 'Other Photo'],
   },
- 
+
   showPopup() {
-    this.setData({ showPopup: true });
+    this.setData({
+      showPopup: true
+    });
   },
 
   onPopupClose() {
-    this.setData({ showPopup: false });
-},
+    this.setData({
+      showPopup: false
+    });
+  },
+
+  onFilenumchange(e) {
+    this.setData({
+      file_num: e.detail
+    })
+  },
+
   onPickerChange(e) {
     console.log(e.detail.value);
-    const { picker, value, index } = e.detail;
+    const {
+      picker,
+      value,
+      index
+    } = e.detail;
     this.setData({
       pickvalue: value
     });
-    // Toast(`当前值：${value}, 当前索引：${index}`);
   },
+
   onPickerCancel(e) {
-    this.setData({ showPopup: false });
+    this.setData({
+      showPopup: false
+    });
   },
+
   onPickerConfirm(e) {
-    const { value, index } = e.detail;
-    // Toast(`当前值：${value}, 当前索引：${index}`);
-    this.setData({ 
+    const {
+      value,
+      index
+    } = e.detail;
+    this.setData({
       showPopup: false,
       pickvalue: value
     });
   },
 
-  onChange(event) {
-    const { picker, value, index } = event.detail;
-    Toast(`当前值：${value}, 当前索引：${index}`);
-  },
 
   afterRead(event) {
-    const {file} = event.detail; // 当设置 mutiple 为 true 时, file 为数组格式，否则为对象格式
+    const {
+      file
+    } = event.detail; // 当设置 mutiple 为 true 时, file 为数组格式，否则为对象格式
+    console.log(file);
     this.setData({
       fileList: file,
-      columns: ['一般货物照片', '破损货物照片', '签收文件照片', '其他'],
     })
-    // this.uploadImg(file);
   },
 
   // 删除图片
@@ -66,21 +81,20 @@ Page({
     })
     // this.uploadImg(fileList)
   },
-  uploadReportPic: function () {
+
+  uploadPic: function () {
     let that = this;
     let imgArr = that.data.fileList;
+    let file_num = that.data.file_num;
+    let pickvalue = that.data.pickvalue;
     console.log(imgArr.length);
     if (imgArr.length > 0) {
-      // 成功个数
-      var successUp = 0;
-      // 失败个数
-      var failUp = 0;
-      // 文件总数
-      var length = imgArr.length;
-      // 第几张
-      var count = 0; 
+      var successUp = 0; // 成功个数
+      var failUp = 0; // 失败个数
+      var length = imgArr.length; // 文件总数
+      var count = 0; // 第几张
       // 上传函数
-      that.uploadImg(imgArr, successUp, failUp, count, length);
+      that.uploadImg(imgArr, file_num, pickvalue, successUp, failUp, count, length);
     } else {
       wx.showToast({
         title: '内容不能为空',
@@ -90,50 +104,46 @@ Page({
       return false;
     }
   },
-  uploadImg:function(imgPaths, successUp, failUp, count, length) {
+
+  uploadImg: function (imgArr, file_num, pickvalue, successUp, failUp, count, length) {
     var that = this;
     console.log("uploadimg that");
     console.log(that);
     wx.showLoading({
       title: '正在上传第' + count + '张',
     })
-      wx.uploadFile({
-        url:'https://www.astoc.tk:4431/mo/upload/upload.php', //写自己的路径
-        filePath: imgPaths[count].url,
-        name: 'file',
-        formData: {
-          'user': 'test1',
-          'file_num' : that.file_num,
-          'pic_type' : that.pickvalue,
-        },
-        success: function (res) {
-          console.log(res)
-          successUp++;
+    wx.uploadFile({
+      url: 'https://www.astoc.tk:4431/mo/upload/upload.php', //写自己的路径
+      filePath: imgArr[count].url,
+      name: 'file',
+      formData: {
+        'user': 'test1',
+        'file_num': file_num,
+        'pic_type': pickvalue,
+      },
+      success: function (res) {
+        console.log(res)
+        successUp++;
+      },
+      fail: function (res) {
+        failUp++;
+      },
+      complete: function (res) {
+        // 下一张
+        count++;
+        if (count == length) {
+          wx.showToast({
+            title: '上传成功',
+            icon: 'success',
+            duration: 2000
+          });
+        } else {
+          // 递归调用，上传下一张
+          that.uploadImg(imgArr, file_num, pickvalue, successUp, failUp, count, length);
+        }
+      }
+    })
 
-          // const { fileList = [] } = _this.data.fileList;
-          // fileList.push({ ...file, url: res.data });
-          // _this.setData({ fileList });
-        },
-        fail: function (res) {
-          failUp++;
-        },
-        complete: function (res) {
-          // 下一张
-          count++;
-          if (count == length) {
-            wx.showToast({
-              title: '上传成功',
-              icon: 'success',
-              duration: 2000
-            });
-          } else {
-            // 递归调用，上传下一张
-            that.uploadImg(imgPaths, successUp, failUp, count, length);
-          }
-        },
-        
-      })    
-    },
   },
   /**
    * 生命周期函数--监听页面加载
